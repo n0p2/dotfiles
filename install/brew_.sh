83ff065d8e91a397da1bf3a427e4b64eaf57af3d
n0p2 <quest.lee@gmail.com>
2017-05-15 17:42:31 -0700

    uncomment

--- install/brew_.sh
+++ install/brew_.sh
@@ -6,14 +6,14 @@
 which -s brew
 if [[ $? != 0 ]]; then
   echo "brew install..."
-  #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
   echo "brew update..."
   #brew update
-  #brew update && brew upgrade
+  brew update && brew upgrade
 fi
 
-# todo, or update
+# TODO update or install
 brew install git
 brew install tmux
 
